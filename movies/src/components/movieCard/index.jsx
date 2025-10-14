d44d47d7916e783bbdb188724c005182c5518d3e
Dylan <20106847@mail.wit.ie>
2025-10-14 12:59:47 +0000

    Card icons - make action icons on movie card configurable

--- movies/src/components/movieCard/index.jsx
+++ movies/src/components/movieCard/index.jsx
@@ -16,7 +16,7 @@ import Grid from "@mui/material/Grid";
 import img from '../../images/film-poster-placeholder.png'
 import Avatar from '@mui/material/Avatar';
 
-export default function MovieCard({movie}) {
+export default function MovieCard({movie, action}) {
 
  const { favorites, addToFavorites } = useContext(MoviesContext);
 
@@ -73,15 +73,15 @@ export default function MovieCard({movie}) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-          
-         <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-        </IconButton>
+      
+        {action(movie)}
+      
         <Link to={`/movies/${movie.id}`}>
-        <Button variant="outlined" size="medium" color="primary">
-          More Info ...
-        </Button>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
         </Link>
+        
       </CardActions>
     </Card>
   );
--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -3,6 +3,7 @@ import { getMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 
 const HomePage = (props) => {
 
